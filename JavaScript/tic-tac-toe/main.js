9acc8a956aabe52f68c7f6e9551eee300ff2cb54
Annie <anniekao@posteo.net>
2019-06-23 15:11:29 -0400

    Added takeTurn feature

--- JavaScript/tic-tac-toe/main.js
+++ JavaScript/tic-tac-toe/main.js
@@ -1,6 +1,6 @@
 // Player objects
-const Player = (name) => {
-    return {name};
+const Player = (name, mark) => {
+    return {name, mark};
 };
 
 // Gameboard
@@ -42,37 +42,42 @@ const render = (board) => {
 
 // Reset board
 
+
 // Functions to allow players to add marks to specific spots (if not already chosen)
 const makeMark = (e, arrPos, boardPos, board) => {
     e.preventDefault();
-    console.log(board);
-
-    // who's turn?
-    let mark = "X";
-
+ 
     let cell = document.getElementById(boardPos);
     if (cell.innerHTML == ""){
-        cell.innerHTML = mark;
-    }
-    let x = arrPos[0];
-    let y = arrPos[1];
-    board[x][y] = mark;   
+        cell.innerHTML = nextTurn;
+        let x = arrPos[0];
+        let y = arrPos[1];
+        board[x][y] = nextTurn; 
+        takeTurn();
+    }  
 };
 
 // Function that determines a winner
 
 // Game control
 const Play = (p1, p2) => {
-    const player1 = Player(p1);
-    const player2 = Player(p2);
+    const player1 = Player(p1[0], p1[1]);
+    const player2 = Player(p2[0], p2[1]);
     const board = Gameboard().playingField();
-    
     return {player1, player2, board};
 };
 
-const p1 = "Doug";
-const p2 = "Bob";
+const p1 = ["Annie", "X"];
+const p2 = ["Bob", "O"];
 const gameOn = Play(p1, p2);
 render(gameOn.board);
 
-
+//Take turn
+var nextTurn = gameOn.player1.mark;
+const takeTurn = () => {
+    if (nextTurn === "O") {
+        nextTurn = "X";
+    } else {
+        nextTurn = "O";
+    }
+};
