fc50eea73956590e24c72bda79571bca4246e8e9
Annie <anniekao@posteo.net>
2019-06-23 11:09:33 -0400

    Added feature to add mark to board and array

--- JavaScript/tic-tac-toe/main.js
+++ JavaScript/tic-tac-toe/main.js
@@ -6,9 +6,9 @@ const Player = (name) => {
 // Gameboard
 const Gameboard = () => {
     let playingField = () => [
-        [0,0,0], 
-        [0,0,0], 
-        [0,0,0]
+        ["","",""], 
+        ["","",""], 
+        ["","",""]
     ];
     return {playingField};
 };
@@ -16,10 +16,23 @@ const Gameboard = () => {
 // Render to webpage
 const render = (board) => {
     let table = document.querySelector('table');
+    let cellId = 1;
     for (var i = 0 ; i < board.length; i++){
         let row = table.insertRow();
         for (var j = 0; j < board[i].length; j++){
-            let cell = row.insertCell()
+            let cell = row.insertCell();
+            // Content position on the board
+            cell.id = cellId; cellId++;
+            // Data position in the array
+            cell.setAttribute('data-row', i);
+            cell.setAttribute('data-cell', j);
+            cell.addEventListener('click', e => {
+                let rowId = cell.getAttribute('data-row');
+                let cellId = cell.getAttribute('data-cell');
+                let arrPos = [rowId, cellId];
+                let boardPos = cell.id;
+                makeMark(e, arrPos, boardPos, board);
+            });
             let text = document.createTextNode = board[i][j];
             cell.append(text);
             row.append(cell);
@@ -27,26 +40,39 @@ const render = (board) => {
     }
 };
 
-// Update board
+// Reset board
 
 // Functions to allow players to add marks to specific spots (if not already chosen)
-const chooseSquare = (board) => {
-    
-}
+const makeMark = (e, arrPos, boardPos, board) => {
+    e.preventDefault();
+    console.log(board);
 
-// Function that determines a win
+    // who's turn?
+    let mark = "X";
+
+    let cell = document.getElementById(boardPos);
+    if (cell.innerHTML == ""){
+        cell.innerHTML = mark;
+    }
+    let x = arrPos[0];
+    let y = arrPos[1];
+    board[x][y] = mark;   
+};
+
+// Function that determines a winner
 
 // Game control
 const Play = (p1, p2) => {
     const player1 = Player(p1);
     const player2 = Player(p2);
-
     const board = Gameboard().playingField();
+    
     return {player1, player2, board};
 };
 
 const p1 = "Doug";
 const p2 = "Bob";
 const gameOn = Play(p1, p2);
+render(gameOn.board);
 
 
