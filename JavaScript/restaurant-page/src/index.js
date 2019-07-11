9c3b54aa91b8c4cbaa53aa3d0738a3c92ba2d115
Annie <anniekao@posteo.net>
2019-07-10 22:07:05 -0400

    Added restaurant copy text

--- JavaScript/restaurant-page/src/index.js
+++ JavaScript/restaurant-page/src/index.js
@@ -1,7 +1,8 @@
-import { loadLogo, loadHeader } from './loader.js';
+import { loadHeader, addCopyText } from './loader.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 loadHeader(); 
+addCopyText();
 
 
 
--- JavaScript/restaurant-page/src/loader.js
+++ JavaScript/restaurant-page/src/loader.js
@@ -11,6 +11,7 @@ function loadHeader() {
 
   const headerLogo = new Image();
   headerLogo.src = Logo;
+  headerLogo.width = '300';
   
   header.appendChild(headerLogo);
   title.append(text);
@@ -19,12 +20,19 @@ function loadHeader() {
   page.appendChild(header);
 }
 
+function addCopyText() {
+  const section = document.createElement('section');
+  const article = document.createElement('article');
+  const copy = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+  const text = document.createTextNode(copy);
+
+  article.append(text);
+  section.appendChild(article)
+  page.appendChild(section);
+}
+
 function createTabs() {
 
 }
 
-function createFooter() {
-
-}
-
-export { loadHeader };
+export { loadHeader, addCopyText };
