7c05e180a491eca9d61c21dbcfe9bc510935ab59
Annie <anniekao@posteo.net>
2019-07-11 12:13:46 -0400

    Added tab modules

---
+++ JavaScript/restaurant-page/src/home-tab.js
@@ -0,0 +1,13 @@
+function renderHomeTab() {
+  const section = document.createElement('section');
+  const article = document.createElement('article');
+  const copy = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+  const text = document.createTextNode(copy);
+
+  article.append(text);
+  section.appendChild(article);
+
+  return section;
+}
+
+export { renderHomeTab };
